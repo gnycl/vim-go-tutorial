4638ee57773baceffd034d02867261f0893afc89
Remy Abdullahi <gnycl@users.noreply.github.com>
2019-04-08 06:02:51 -0500

    learning about vim-go

--- main.go
+++ main.go
@@ -3,5 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("vim-go")
+	fmt.Println("Hello remy!")
+}
+
+func Bar() string {
+	return "bar"
+}
+
+func Foo() string {
+	return "foo"
+}
+
+func Qux(v string) string {
+	if v == "foo" {
+		return Foo()
+	}
+
+	if v == "bar" {
+		return Bar()
+	}
+
+	return "INVALID"
 }
